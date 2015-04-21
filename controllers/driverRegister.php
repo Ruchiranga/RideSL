86b2bb28df98e811ca0e9ec966b3ba480a7b7fcd
Ruchiranga <truchiranga@gmail.com>
2015-04-21 19:08:04 +0530

    Fixed some issues in session mgt

--- controllers/driverRegister.php
+++ controllers/driverRegister.php
@@ -4,6 +4,14 @@ class driverRegister extends Controller {
 
     function __construct() {
         parent::__construct();
+        Session::init();
+        $logged = Session::get('loggedIn');
+        $privilege = Session::get('privilege');
+        if ($logged == false || $privilege != 'd') {
+            Session::destroy();
+            header('location: '.URL.'login');
+            exit();
+        }
     }
 
     public function index() {

34f19a6d3fec2a48feb85b1840cb26d831791303
anuradha1992 <anu.welivita@gmail.com>
2015-04-22 22:00:13 +0530

    added profile info to editvehicle page

--- models/vehicleRegister_model.php
+++ models/vehicleRegister_model.php
@@ -7,6 +7,8 @@
  */
 
 class vehicleRegister_Model extends Model {
+    
+    private $username;
 
     function __construct() {
         parent::__construct();
@@ -25,6 +27,19 @@ class vehicleRegister_Model extends Model {
             $data = $sth->fetch();
             $_SESSION['owner_id'] = $data['owner_id'];
             return $data;
+        } else {
+            return NULL;
+        }
+    }
+    
+    public function getPhoneNoList() {
+        $sth = $this->db->prepare("Select p.telephone_number from owner o inner join telephone_number p on p.owner_id = o.owner_id where o.owner_id = '" . $_SESSION['owner_id'] . "'");
+        $sth->execute();
+
+        $count = $sth->rowCount();
+
+        if ($count > 0) {
+            return $sth->fetchAll();
         } else {
             return NULL;
         }
@@ -56,6 +71,65 @@ class vehicleRegister_Model extends Model {
             }
         } else {
             return NULL;
+        }
+    }
+    
+    public function addPhoneNo($phoneNo) {
+
+        $sth = $this->db->prepare("insert into telephone_number values ('" . $_SESSION['owner_id'] . "', '" . $phoneNo . "')");
+
+        $sth->execute();
+
+        if ($sth->rowCount() == 1) {
+            echo 'updated successfully';
+        } else {
+            echo 'update failed';
+        }
+    }
+
+    public function editName($firstName, $lastName) {
+
+        $sth = $this->db->prepare("update owner set first_name = '" . $firstName . "', last_name = '" . $lastName . "' where owner_id = '" . $_SESSION['owner_id'] . "'");
+
+        $sth->execute();
+
+        if ($sth->rowCount() == 1) {
+            echo 'updated successfully';
+        } else {
+            echo 'update failed';
+        }
+    }
+
+    public function editEmail($email) {
+
+        $sth = $this->db->prepare("update owner set email = '" . $email . "' where owner_id = '" . $_SESSION['owner_id'] . "'");
+
+        $sth->execute();
+
+        if ($sth->rowCount() == 1) {
+            echo 'updated successfully';
+        } else {
+            echo 'update failed';
+        }
+    }
+    
+    public function updatePhoneNo($owner_id, $old_phone_no, $new_phone_no) {
+        $sth = $this->db->prepare("update telephone_number set telephone_number = '" . $new_phone_no . "' where telephone_number = '" . $old_phone_no . "' and owner_id='" . $owner_id . "'");
+        $sth->execute();
+        if ($sth->rowCount() >= 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function dltPhoneNo($owner_id, $phone_no) {
+        $sth = $this->db->prepare("delete from telephone_number where telephone_number = '" . $phone_no . "' and owner_id='" . $owner_id . "'");
+        $sth->execute();
+        if ($sth->rowCount() >= 1) {
+            return true;
+        } else {
+            return false;
         }
     }
 
